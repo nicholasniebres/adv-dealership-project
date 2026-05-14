6a562a2c6a2ccb5fd51a14d0150a81812ce8f238
nicholasniebres <nicholasniebres97@gmail.com>
2026-05-14 14:15:09 -0700

    The perfect menu2

--- src/main/java/com/skills4it/dealership/ui/UserInterface.java
+++ src/main/java/com/skills4it/dealership/ui/UserInterface.java
@@ -41,7 +41,7 @@ public class UserInterface {
     private void displayHeader() {
         System.out.println();
         System.out.println("=============================================");
-        System.out.println(dealership.getName());
+        System.out.println("Welcome to " + dealership.getName());
         System.out.println(dealership.getAddress() + " | " + dealership.getPhone());
         System.out.println("=============================================");
     }
